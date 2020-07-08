b63772f94ba117b18541b20b7c306744cb132749
kamil-markowski <kamilszmarkowski@gmail.com>
2020-07-08 22:59:41 +0200

    Rest api created for add event and list of event logs

--- src/main/java/com/isa/servlet/AddTest.java
+++ src/main/java/com/isa/servlet/AddTest.java
@@ -1,4 +1,56 @@
 package com.isa.servlet;
 
-public class AddTest {
+import com.isa.domain.EventInLog;
+import com.isa.service.EventInLogService;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add-event")
+public class AddTest extends HttpServlet {
+
+//    @EJB
+////            (lookup = java:jboss/exported/jdd-coach/EventInLogRepositoryBean!engine.repository.EventInLogRepositoryRemote);
+//            (lookup="java:global/jdd-coach/EventInLogRepositoryBean!engine.repository.EventInLogRepositoryRemote")
+//    private EventInLogRepositoryRemote eventInLogRepositoryRemote;
+
+//    @Inject
+//    RecordDownloader recordDownloader;
+
+    @Inject
+    EventInLogService eventInLogService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+//        JsonEventInLog jsonEventInLog = recordDownloader.getRecord();
+//
+//        EventInLog eventInLog = null;
+//        eventInLog.setId(Long.parseLong(jsonEventInLog.getId()));
+//        eventInLog.setCoachInfoLink(jsonEventInLog.getCoachInfoLink());
+//        eventInLog.setEventName(jsonEventInLog.getEventName());
+//        eventInLog.setEventDate(jsonEventInLog.getEventDate());
+//
+//        eventInLogService.save(eventInLog);
+
+        EventInLog eventInLog = new EventInLog();
+        eventInLog.setIp("test ip 1.0.0.0.0");
+        eventInLog.setCoachInfoLink("link test");
+        eventInLog.setEventName("test name");
+        eventInLog.setEventDate("test date");
+
+        eventInLogService.save(eventInLog);
+
+//        resp.sendRedirect("/");
+    }
 }
+
+
+
